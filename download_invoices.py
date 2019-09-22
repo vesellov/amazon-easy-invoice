84ac14f10158fdea3734744b896696987f73e728
Veselin Penev <penev.veselin@gmail.com>
2019-09-22 15:25:18 +0200

    try to reuse existing session if possible

--- download_invoices.py
+++ download_invoices.py
@@ -1,41 +1,90 @@
 import os
 import re
 import time
+import logging
+import optparse
+
+logging.basicConfig(level=logging.INFO)
+
 from urllib import parse as urlparse
 
 from selenium import webdriver
 
 import config
 
-browser = webdriver.Chrome()
+browser = None
 
 
 def login():
+    global browser
+
+    if browser:
+        logging.info('close previous session before login')
+        try:
+            browser.quit()
+        except:
+            pass
+
+    logging.info('login')
+    browser = webdriver.Chrome()
+    logging.info(f'new browser : {browser.command_executor._url} {browser.session_id}')
+
+    open('session', 'w').write('%s %s' % (browser.command_executor._url, browser.session_id))
+
     browser.get(config.AMAZON_LOGIN_URL)
     time.sleep(5)
 
     # Fill username
+    logging.info('email')
     email = browser.find_element_by_name("email")
     email.send_keys(config.AMAZON_USER_EMAIL)
     email.submit()
     time.sleep(5)
 
     # Fill password
+    logging.info('password')
     password = browser.find_element_by_name("password")
     password.send_keys(config.AMAZON_USER_PASSWORD)
     password.submit()
     time.sleep(45)
 
 
-def download_invoices_with_tracking_ids_as_pdf():
-    login()
+def join():
+    global browser
 
-    browser.get(config.AMAZON_ORDERS_URL)
+    if browser:
+        logging.info('close previous session before join')
+        try:
+            browser.quit()
+        except:
+            pass
+
+    try:
+        known_url, session_id = open('session', 'r').read().split(' ')
+        browser = webdriver.Remote(command_executor=known_url, desired_capabilities={})
+        browser.session_id = session_id
+        logging.info(f'joined browser : {browser.command_executor._url} {browser.session_id}')
+    except:
+        try:
+            os.remove('session')
+        except:
+            pass
+        login()
+
+
+def download_invoices_with_tracking_ids_as_pdf():
+    try:
+        browser.get(config.AMAZON_ORDERS_URL)
+    except:
+        login()
+
     time.sleep(5)
     order_urls_length = len(browser.find_elements_by_xpath("//a[contains(@href, 'progress-tracker')]"))
 
     if not order_urls_length:
-        browser.quit()
+        logging.info('no orders found')
+        # browser.quit()
+        return
 
     orders = []
 
@@ -67,6 +116,7 @@ def download_invoices_with_tracking_ids_as_pdf():
 
         orders.append({order_id: {"tracking_id": tracking_id, "delivery_by": delivery_by}})
 
+        logging.info('browser.back')
         browser.back()
         browser.implicitly_wait(5)
 
@@ -82,6 +132,7 @@ def download_invoices_with_tracking_ids_as_pdf():
         delivery_by = orders[i][order_id]["delivery_by"]
         html_file = f'{here}/Downloads/invoice_{order_id}.html'
 
+        logging.info('browser.get order_id=%r', order_id)
         browser.get(config.AMAZON_ORDER_INVOICE_URL + order_id)
 
         with open(html_file, 'wb') as f:
@@ -92,8 +143,18 @@ def download_invoices_with_tracking_ids_as_pdf():
             ).encode('utf-8')
             f.write(page_content_encoded)
 
-    browser.quit()
+    logging.info('DONE')
+    # browser.quit()
 
 
 if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option("-l", "--login", default=False, dest="login", action='store_true', help='open a new browser window and start sign in process')
+    (opt, args) = parser.parse_args()
+
+    if opt.login:
+        login()
+    else:
+        join()
+
     download_invoices_with_tracking_ids_as_pdf()
